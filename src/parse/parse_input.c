c009b96eaa1ee3496f5e81bb47f843aad69d5918
eduaserr <157403329+eduaserr@users.noreply.github.com>
2024-12-17 16:46:00 +0100

    lib makefile bonus rule added for compilation, parse input update isspace

--- src/parse/parse_input.c
+++ src/parse/parse_input.c
@@ -6,7 +6,7 @@
 /*   By: eduaserr < eduaserr@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 14:18:18 by eduaserr          #+#    #+#             */
-/*   Updated: 2024/12/13 18:01:22 by eduaserr         ###   ########.fr       */
+/*   Updated: 2024/12/17 16:41:43 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int	check_extension(char **argv)
 
 	len = ft_strlen(argv[1]);
 
+	while (argv[1][len - 1] && ft_isspace(argv[1][len - 1]))
+		len--;
 	if (ft_strncmp(&argv[1][len - 4], ".ber", 4) == 0)
 		return (0);
 	return (1);
