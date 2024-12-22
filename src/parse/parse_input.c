2eaf23a24c13a8031e05f26af660b18329de99c8
eduaserr <eduaserr@github.com>
2024-12-22 01:53:04 +0100

    parse lines and com¡lumns finish

--- src/parse/parse_input.c
+++ src/parse/parse_input.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_input.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eduaserr < eduaserr@student.42malaga.co    +#+  +:+       +#+        */
+/*   By: eduaserr <eduaserr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 14:18:18 by eduaserr          #+#    #+#             */
-/*   Updated: 2024/12/20 20:08:22 by eduaserr         ###   ########.fr       */
+/*   Updated: 2024/12/22 01:51:39 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,28 +23,39 @@ int	check_extension(char **argv)
 		return (0);
 	return (1);
 }
+
 void	line_edges(t_game *game)
 {
 	char	**map;
 	int		i;
-	int		j;
 
-	j = 0;
 	i = 0;
 	map = game->map.map;
 	
-	while (map[0][i])
+	while (map[0][i] && map[game->map.length - 1][i])
 	{
-		if (map[0][i] == '1')
+		if (map[0][i] == '1' && map[game->map.length - 1][i] == '1')
 			i++;
 		else
-			ft_error("Must has to be surrounded by walls\n");
+			ft_error("Map must has to be surrounded by walls\n");
 	}
 }
 
 void	column_edges(t_game *game)
 {
-	(void)game;
+	char	**map;
+	int		i;
+
+	i = 0;
+	map = game->map.map;
+	
+	while (map[i][0] && map[i][game->map.width - 1])
+	{
+		if (map[i][0] == '1' && map[i][game->map.width - 1] == '1')
+			i++;
+		else
+			ft_error("Map must has to be surrounded by walls\n");
+	}
 }
 
 void	check_limits(t_game *game)
