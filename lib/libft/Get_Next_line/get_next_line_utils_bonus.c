1141a4e6bb6ef7b3b717605fe9bc97e2796dbe54
eduaserr <157403329+eduaserr@users.noreply.github.com>
2025-01-10 16:45:24 +0100

    Fixed segfault (Comprobacion de que s1/s2 existen)

--- lib/libft/Get_Next_line/get_next_line_utils_bonus.c
+++ lib/libft/Get_Next_line/get_next_line_utils_bonus.c
@@ -6,11 +6,12 @@
 /*   By: eduaserr < eduaserr@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 12:05:59 by eduaserr          #+#    #+#             */
-/*   Updated: 2024/07/25 12:25:54 by eduaserr         ###   ########.fr       */
+/*   Updated: 2025/01/10 16:06:46 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+#include <stdio.h>
 
 char	*ft_substr_gnl(char *str, ssize_t start, ssize_t len)
 {
@@ -46,9 +47,9 @@ char	*ft_strjoin_gnl(char *s1, char *s2)
 	s3 = malloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!s3)
 		return (NULL);
-	while (s1[++i])
+	while (s1 && s1[++i])
 		s3[i] = s1[i];
-	while (s2[++j])
+	while (s2 && s2[++j])
 		s3[i++] = s2[j];
 	s3[i] = '\0';
 	return (ft_free_str(&s1), s3);
