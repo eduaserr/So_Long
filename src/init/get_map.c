4b3bbba4c94655e7105a62c2b4c0849ca60c2e07
eduaserr <157403329+eduaserr@users.noreply.github.com>
2025-01-10 17:14:50 +0100

    Remade get_map functions (improve legibility)

--- src/init/get_map.c
+++ src/init/get_map.c
@@ -6,13 +6,13 @@
 /*   By: eduaserr < eduaserr@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 15:51:25 by eduaserr          #+#    #+#             */
-/*   Updated: 2025/01/08 19:03:00 by eduaserr         ###   ########.fr       */
+/*   Updated: 2025/01/10 16:44:03 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/so_long.h"
 
-static char	*get_line(int fd)
+static char *get_line(int fd, int *len)
 {
 	char	*superline;
 	char	*line;
@@ -21,23 +21,49 @@ static char	*get_line(int fd)
 	superline = NULL;
 	line = get_next_line(fd);
 	if (!line)
-	{
-		ft_error("Empty map error\n");
-	}
-	size_line = ft_strlen_sl(line);
+		size_line = 0;
+	else
+		size_line = ft_strlen(line);
+	if (size_line)
+		*len = 1;
 	while (line)
 	{
+		*len += 1;
 		superline = ft_strjoin_gnl(superline, line);
-		free(line);
+		ft_free_str(&line);
 		line = get_next_line(fd);
-		if (line && size_line != ft_strlen_sl(line))
-		{
-			free(line);
-			free(superline);
-			ft_error("Size map error\n");
-		}
 	}
+	superline = ft_strjoin_gnl(superline, line);
+	ft_free_str(&line);
 	return (superline);
+}
+
+static char	**check_superline(char *superline, int len)
+{	
+	char **map;
+	int	i;
+	int len_check;
+	int len_current;
+	
+	if (!superline || !superline[0])
+		return (free(superline), ft_error("Empty map"), NULL);
+	map = ft_split(superline, '\n');
+	i = 0;
+	if (map)
+		len_check = ft_strlen(map[i]);
+	else
+		return (free(superline), ft_error("Only newlines, empty map"), NULL);
+	while (map[++i])
+	{
+		len_current = ft_strlen(map[i]);
+		if (len_check != len_current)
+			return (free(superline), ft_freemap(map), ft_error("Map not rectangular"), NULL);
+	}
+	if (i > 0 && ft_strlen(map[i]) == 0)
+		len--;
+	if (len != i)
+		return (ft_freemap(map), free(superline), ft_error("Empty line"), NULL);
+	return (map);
 }
 
 static char	**read_map(char *file_map)
@@ -45,19 +71,16 @@ static char	**read_map(char *file_map)
 	char	**map;
 	char	*superline;
 	int		fd;
+	int		len;
 
 	superline = NULL;
+	len = 0;
 	fd = open(file_map, O_RDONLY);
 	if (fd < 0)
 		ft_error("Open fd error\n");
-	superline = get_line(fd);
-	if (superline[0] == '\n')
-	{
-		free(superline);
-		ft_error("Line error\n");
-	}
+	superline = get_line(fd, &len);
 	close(fd);
-	map = ft_split(superline, '\n');
+	map = check_superline(superline, len);
 	free(superline);
 	return (map);
 }
@@ -75,7 +98,7 @@ void	check_map(t_game *game, char *file_map)
 	init_struct(game);
 	game->map.map = read_map(file_map);
 	get_fullmatrixlen(game->map.map, &game->map.length, &game->map.width);
-	game->map.cpymap = ft_arrdup(game->map.map, game->map.width, game->map.length);
+	game->map.cpymap = ft_arrdup(game->map.map);
 	if (!game->map.cpymap)
 	{
 		ft_freegame(game);
