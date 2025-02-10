d65b52f3501281ec8c2c2890673e55b1320621b0
eduaserr <eduaserr@eduaserr>
2025-02-10 17:16:57 +0100

    check_path fix, fckin flood fill yes yes skibidi dob

--- src/init/check_path.c
+++ src/init/check_path.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_path.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eduaserr < eduaserr@student.42malaga.co    +#+  +:+       +#+        */
+/*   By: eduaserr <eduaserr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 01:23:41 by eduaserr          #+#    #+#             */
-/*   Updated: 2025/02/04 17:03:35 by eduaserr         ###   ########.fr       */
+/*   Updated: 2025/02/10 17:00:15 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,17 +58,16 @@ void	exit_pos(t_map *map, char **filemap)
 
 int	check_path(t_map *map, char **cpymap, int y, int x)
 {
-	if (!cpymap || y < 0 || y >= map->length
-		|| x < 0 || x >= map->width || !cpymap[y])
+	if (!cpymap || !cpymap[y])
 		return (0);
 	if (cpymap[y][x] == 'E' && map->exit == 1)
 		map->exit--;
 	if (cpymap[y][x] == 'C')
 		map->coin--;
-	if (cpymap[y][x] == '1' || cpymap[y][x] == 'V' || cpymap[y][x] == 'E')
-		return (0);
 	if (map->coin == 0 && map->exit == 0)
 		return (1);
+	if (cpymap[y][x] == '1' || cpymap[y][x] == 'V' || cpymap[y][x] == 'E')
+		return (0);
 	cpymap[y][x] = 'V';
 	if (check_path(map, cpymap, y + 1, x)
 		|| check_path(map, cpymap, y - 1, x)
